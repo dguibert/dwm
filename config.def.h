73be9b412afd309ca3289c4527503ef20d1ad543
David Guibert <david.guibert@gmail.com>
2016-04-22 18:22:25 +0200

    config: support shortcuts for vbox inside windows

--- config.def.h
+++ config.def.h
@@ -72,6 +72,14 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,         incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,         setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,         setmfact,       {.f = +0.05} },
+        // support for virtualbox inside Windows
+	{ MODKEY,                       XK_Up,        focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_Down,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_KP_Add,      incnmaster,   {.i = +1 } },
+	{ MODKEY,                       XK_KP_Subtract, incnmaster,   {.i = -1 } },
+	{ MODKEY,                       XK_Left,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_Right,     setmfact,       {.f = +0.05} },
+
 	{ MODKEY,                       XK_Return,    zoom,           {0} },
 	{ MODKEY,                       XK_Tab,       view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,         killclient,     {0} },
